a9a064378006dd075c218b898dd0e49f9be59a0b
Troy Bick <tbick00@gmail.com>
2025-08-01 22:30:13 -0500

    Configured TODOs, kindof. Also edited main to print cws using std::env.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,25 @@
 //  Command ran - intended to use as a shortcut tool to start the dev environment for custom development
 
 
+use std::env;
+use std::process::{Command, Stdio};
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    //  Entry Point
+
+
+    //  What do I want to do on blank startup? What are the defaults
+    //      Open VSCode in the current directory
+
+    //  GOALS
+    //      -Parse commands
+
+    // TODO: Check Commands somehow
+
+    let cwd = env::current_dir();
+    println!("{:?}", cwd.unwrap());
+
+
+
 }
